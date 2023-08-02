88f6bad863e27c2b4f8c375bb8c60f582d4ea12b
Heena Trivedi <trivediheenalove@gmail.com>
2023-08-02 23:10:55 +0530

    crud changes

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+                'name' => 'index'
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $data = $request->all();
+        echo '<pre>';
+        print_r($data);
+        $name = "test";
+        $description = "testing";
+        $product = Product::create(["name"=>$name,"description"=>$description]);
+        return response()->json([
+                'status' => '200',
+                "data"=>$product
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        echo '<pre>';
+        print_r($data);
+        $name = "test";
+        $description = "testing";
+        $product = Product::create(["name"=>$name,"description"=>$description]);
+        return response()->json([
+                'status' => '200',
+                "data"=>$product
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json([
+                'name' => 'show'
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return response()->json([
+                'name' => 'edit'
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return response()->json([
+                'name' => 'update'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return response()->json([
+                'name' => 'destroy'
+        ]);
+    }
+}
